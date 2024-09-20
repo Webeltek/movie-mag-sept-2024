c9da272f4327398a32f81a3585add0c140944e9d
Velislav Velikov <50297857+Webeltek@users.noreply.github.com>
2024-09-20 14:37:54 +0200

    render home page

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ const app = express();
 app.engine('hbs', handlebars.engine({
     extname: 'hbs'
 }))
-
 app.set('view engine', 'hbs');
 app.set('views', './src/views')
+app.use(express.static('public'))
 
 app.get('/', (req,res)=> {
-    res.render('index')
+    res.render('home')
 })
 
 app.listen(5000, ()=> console.log('Server is listening on http://localhost:5000...'));
