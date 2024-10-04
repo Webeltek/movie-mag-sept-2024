95510e19c3bd840cd338c2dd056dbe76a55358a0
Velislav Velikov <50297857+Webeltek@users.noreply.github.com>
2024-10-05 00:11:58 +0200

    hash password before save

--- src/models/User.js
+++ src/models/User.js
@@ -1,9 +1,19 @@
 import { Schema, model} from 'mongoose'
+import bcrypt from 'bcrypt'
 
 const userSchema = new Schema({
     email: String,
     password: String
 });
+
+const SALT_ROUNDS = 10;
+
+// Hash password before save
+userSchema.pre('save', async function(){
+    //TODO: Hash password
+    const hash= await bcrypt.hash(this.password, SALT_ROUNDS)
+    this.password = hash;
+})
 
 const User = model('User', userSchema);
 
--- src/services/authService.js
+++ src/services/authService.js
@@ -4,7 +4,6 @@ import User from '../models/User.js'
 const register = (email,password)=>{
     return User.create({email,password});
     
-    
 }
 
 export default {
