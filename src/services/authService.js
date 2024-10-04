32649cbc919741299a1fe9434fa956867287c743
Velislav Velikov <50297857+Webeltek@users.noreply.github.com>
2024-10-04 13:32:49 +0200

    basic user creation in db

---
+++ src/services/authService.js
@@ -0,0 +1,12 @@
+import User from '../models/User.js'
+//TODO create model
+
+const register = (email,password)=>{
+    return User.create({email,password});
+    
+    
+}
+
+export default {
+    register
+}
